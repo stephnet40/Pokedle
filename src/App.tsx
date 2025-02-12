cf5c5e1b043831f23cc5989c27bc12d477075e1f
stephnet40 <stephnet40@gmail.com>
2025-02-12 15:58:53 -0600

    Update styling

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,22 @@ function App() {
     setSearchResults([]);
   } 
 
+  const getImgSrc = (name: string) => {
+    if (name.includes("mime")) {
+      name = name.replace(". ", "-")
+    }
+
+    if (name.includes("'d")) {
+      name = name.replace("'", "")
+    }
+
+    return `sprites/${name}.png`;
+  }
+
+  const formatColors = (colors: string[]) => {
+    return colors.join(", ");
+  }
+
   const formatHeight = (height: number) => {
     const heightArr = height.toFixed(2).toString().split('.');
     return `${heightArr[0]}'${heightArr[1]}"`
@@ -58,21 +74,13 @@ function App() {
   const formatWeight = (weight: number) => {
     return `${weight.toFixed(1)} lbs`
   }
-
-  const getImgSrc = (name: string) => {
-    if (name.includes("mime")) {
-      name = name.replace(". ", "-")
-    }
-
-    return `sprites/${name}.png`;
-  }
   
   return (
     <>
       <div>
-        <div>
+        <div className='search'>
           <input type='text' placeholder='Search' value={searchInput} onChange={handleInputChange}></input>
-          <ul>
+          <ul className='filtered-search'>
             {searchResults.map((result, index) => 
               <li key={index}>
                 <button key={`${index}-btn`} onClick={() => selectPokemon(result)}>{result.name}</button>
@@ -80,15 +88,15 @@ function App() {
           </ul>
         </div>
 
-        <div>
+        <div className='guess-list'>
           {guesses.map((pokemon, index) => 
             <div key={`${index}-${pokemon}`} className='pokemon-clues'>
               <div key={`${index}-${pokemon}-name`}><img src={getImgSrc(pokemon.name)}></img></div>
               <div key={`${index}-${pokemon}-type1`}>{pokemon.type1}</div>
-              <div key={`${index}-${pokemon}-type2`}>{pokemon.type2}</div>
+              <div key={`${index}-${pokemon}-type2`}>{pokemon.type2 ? pokemon.type2 : "None"}</div>
               <div key={`${index}-${pokemon}-evolution-stage`}>{pokemon.evolutionStage}</div>
-              <div key={`${index}-${pokemon}-fully-evolved`}>{pokemon.fullyEvolved}</div>
-              <div key={`${index}-${pokemon}-color`}>{pokemon.color}</div>
+              <div key={`${index}-${pokemon}-fully-evolved`}>{pokemon.fullyEvolved ? "Yes" : "No"}</div>
+              <div key={`${index}-${pokemon}-color`}>{formatColors(pokemon.color)}</div>
               <div key={`${index}-${pokemon}-height`}>{formatHeight(pokemon.height)}</div>
               <div key={`${index}-${pokemon}-weight`}>{formatWeight(pokemon.weight)}</div>
             </div>
